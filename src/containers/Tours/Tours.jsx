618e4a2c5c471a570276cdb8d975bca254d6bb2f
irving Rodriguez <al221810739@gmail.com>
2026-07-07 16:39:22 -0600

    add layout to torus

--- src/containers/Tours/Tours.jsx
+++ src/containers/Tours/Tours.jsx
@@ -3,7 +3,7 @@ import { Button, Grid, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import ToursContext from "../../Context/Tours/ToursContext";
 import TourAdminCard from "../../components/Tours/TourAdminCard";
-
+import Layout from "../../components/Layout/Layout";
 const Tours = () => {
   const { getAllTours, tours } = useContext(ToursContext);
 
@@ -12,27 +12,29 @@ const Tours = () => {
   }, []);
 
   return (
-    <Grid container spacing={2}>
-      <Grid size={12}>
-        <Typography
-          sx={{ color: "white", fontWeight: "bold", fontSize: "30px" }}
-        >
-          Tours
-        </Typography>
-      </Grid>
-      <Grid size={12} sx={{ display: "flex", justifyContent: "end" }}>
-        <Link to='/registrar-tour'>
-          <Button variant='contained' color='primary' size='large'>
-            Agregar
-          </Button>
-        </Link>
-      </Grid>
-      {tours.map((t, index) => (
-        <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={index}>
-          <TourAdminCard tour={t} onEdit={false} onView={true} />
+    <Layout>
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid size={12}>
+          <Typography
+            sx={{ color: "white", fontWeight: "bold", fontSize: "30px" }}
+          >
+            Tours
+          </Typography>
         </Grid>
-      ))}
-    </Grid>
+        <Grid size={12} sx={{ display: "flex", justifyContent: "end" }}>
+          <Link to='/registrar-tour'>
+            <Button variant='contained' color='primary' size='large'>
+              Agregar
+            </Button>
+          </Link>
+        </Grid>
+        {tours.map((t, index) => (
+          <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={index}>
+            <TourAdminCard tour={t} onEdit={false} onView={true} />
+          </Grid>
+        ))}
+      </Grid>
+    </Layout>
   );
 };
 
